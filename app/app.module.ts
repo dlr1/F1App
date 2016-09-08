7a2a203d2730d9ab7ebcadfe82ec71d0f5b2c7e7
Darbha, Leelaram (Contractor) <Leelaram.Darbha@xo.com>
2016-09-08 10:15:54 -0500

    APP_INITIALIZER

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule}      from '@angular/core';
+import {NgModule, APP_INITIALIZER, Injectable}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {routing} from './app.routing';
@@ -8,16 +8,39 @@ import {SharedModule} from './shared/shared.module';
 import {DriverModule} from './driver/driver.module';
 import {FormSamplesModule} from './formSamples/forms.module';
 import {MiscSamplesModule} from './misc/misc.module';
+
 // commented race and team modules to be lazy loaded
 // import {RaceModule} from './race/race.module';
 // import {TeamModule} from './team/team.module';
 
 import {AppComponent}  from './app';
 import {PageNotFoundComponent}  from './pagenotfound';
+import {DataService} from './shared/dataService';
+
+@Injectable()
+export class ConfigService {
+
+        constructor(private _dataService: DataService) {
+          _dataService.someVar = 'this is set in the configureservice when app is initialized';
+        }  
+
+        load(): Promise<string> {            
+          console.log('configservice called')
+            return Promise.resolve('this is some config');
+        }
+    }
 
 @NgModule({
   imports:      [ BrowserModule, routing,  DriverModule, FormSamplesModule, MiscSamplesModule, SharedModule.forRoot()],
   declarations: [ AppComponent,  PageNotFoundComponent ],
+  providers:[ConfigService, DataService,
+    { provide: APP_INITIALIZER,
+              useFactory: (config: ConfigService) => () => config.load(),
+              deps: [ConfigService, DataService], 
+              multi: true }
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
+
+
--- app/shared/dataService.ts
+++ app/shared/dataService.ts
@@ -3,9 +3,17 @@ import { Response,Jsonp, Http } from '@angular/http';
 
 @Injectable()
 export class DataService{
-    constructor(private http:Http){}
-        
+    _someVar = 'default Value';
+    constructor(private http:Http){        
+        console.log(this._someVar);
+    }
+
+    set someVar(val){
+        this._someVar = val;
+        console.log(this._someVar);
+    }  
     getMetadata(){
+        console.log(this._someVar);
         return this.http.get('app/shared/metadata.json').map(x=>x.json());
     }
     getMOP(){
